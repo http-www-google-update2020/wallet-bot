2341afade084ae54be92a3d1eae7db10d418766c
Google update 2020 <azura9999999999@gmail.com>
2020-07-26 20:07:58 +0800

    Initialize bugfix/iotdb-grafana-boolean

---
+++ grafana/src/main/java/org/apache/iotdb/web/grafana/dao/impl/BasicDaoImpl.java
@@ -0,0 +1,200 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iotdb.web.grafana.dao.impl;
+
+import java.sql.DatabaseMetaData;
+import java.time.Duration;
+import org.apache.iotdb.jdbc.Constant;
+import org.apache.iotdb.tsfile.utils.Pair;
+import org.apache.iotdb.web.grafana.bean.TimeValues;
+import org.apache.iotdb.web.grafana.dao.BasicDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+@PropertySource("classpath:application.properties")
+public class BasicDaoImpl implements BasicDao {
+
+  private static final Logger logger = LoggerFactory.getLogger(BasicDaoImpl.class);
+
+  public static final String FALLBACK_AGG_FUNCTION = "LAST";
+
+  private final JdbcTemplate jdbcTemplate;
+
+  private static long timestampRadioX = -1L;
+
+  @Value("${timestamp_precision}")
+  private String timestampPrecision;
+
+  @Value("${isDownSampling}")
+  private boolean isDownSampling;
+
+  @Value("${function}")
+  private String function;
+
+  @Value("${interval}")
+  private String interval;
+
+
+  @Autowired
+  public BasicDaoImpl(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @Override
+  public List<String> getMetaData() {
+    ConnectionCallback<Object> connectionCallback = new ConnectionCallback<Object>() {
+      public Object doInConnection(Connection connection) throws SQLException {
+        DatabaseMetaData databaseMetaData = connection.getMetaData();
+        ResultSet resultSet = databaseMetaData.getColumns(Constant.CATALOG_TIMESERIES, "root.*", "root.*", null);
+        logger.info("Start to get timeseries");
+        List<String> columnsName = new ArrayList<>();
+        while (resultSet.next()) {
+          String timeseries = resultSet.getString(1);
+          columnsName.add(timeseries.substring(5));
+        }
+        return columnsName;
+      }
+    };
+    return (List<String>) jdbcTemplate.execute(connectionCallback);
+  }
+
+  private static void setTimestampRadioX(String timestampPrecision) {
+    switch (timestampPrecision) {
+      case "us":
+        timestampRadioX = 1000;
+        break;
+      case "ns":
+        timestampRadioX = 1000_000;
+        break;
+      default:
+        timestampRadioX = 1;
+    }
+    logger.info("Use timestamp precision {}", timestampPrecision);
+  }
+
+  /**
+   * Note: If the query fails this could be due to AGGREGATIION like AVG on booleayn field.
+   * Thus, we then do a retry with FIRST aggregation.
+   * This should be solved better in the long run.
+   */
+  @Override
+  public List<TimeValues> querySeries(String s, Pair<ZonedDateTime, ZonedDateTime> timeRange) {
+    if(timestampRadioX == -1) {
+      setTimestampRadioX(timestampPrecision);
+    }
+    try {
+      return querySeriesInternal(s, timeRange, function);
+    } catch (Exception e) {
+      logger.info("Execution failed, trying now with FIRST Function!");
+      // Try it with FIRST
+      try {
+        return querySeriesInternal(s, timeRange, FALLBACK_AGG_FUNCTION);
+      } catch (Exception e2) {
+        logger.warn("Even FIRST query did not succeed, returning NULL now", e2);
+        return Collections.emptyList();
+      }
+    }
+  }
+
+  public List<TimeValues> querySeriesInternal(String s, Pair<ZonedDateTime, ZonedDateTime> timeRange, String function) {
+    Long from = zonedCovertToLong(timeRange.left);
+    Long to = zonedCovertToLong(timeRange.right);
+    final long hours = Duration.between(timeRange.left, timeRange.right).toHours();
+    List<TimeValues> rows = null;
+    String sql = String.format("SELECT %s FROM root.%s WHERE time > %d and time < %d",
+        s.substring(s.lastIndexOf('.') + 1), s.substring(0, s.lastIndexOf('.')),
+        from * timestampRadioX, to * timestampRadioX);
+    String columnName = "root." + s;
+    if (isDownSampling && (hours > 1)) {
+      if (hours < 30 * 24 && hours > 24) {
+        interval = "1h";
+      } else if (hours > 30 * 24) {
+        interval = "1d";
+      }
+      sql = String.format(
+          "SELECT " + function
+              + "(%s) FROM root.%s WHERE time > %d and time < %d group by (%s, [%d, %d])",
+          s.substring(s.lastIndexOf('.') + 1), s.substring(0, s.lastIndexOf('.')),
+          from * timestampRadioX, to * timestampRadioX,
+          interval, from * timestampRadioX, to * timestampRadioX);
+      columnName = function + "(root." + s + ")";
+    }
+    logger.info(sql);
+    try {
+      rows = jdbcTemplate.query(sql, new TimeValuesRowMapper(columnName));
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      throw new RuntimeException("Query did not sucees", e);
+    }
+    return rows;
+  }
+
+  private Long zonedCovertToLong(ZonedDateTime time) {
+    return time.toInstant().toEpochMilli();
+  }
+
+  static class TimeValuesRowMapper implements RowMapper<TimeValues> {
+
+    static final String TRUE_STR = "true";
+    static final String FALSE_STR = "false";
+    String columnName;
+
+    TimeValuesRowMapper(String columnName) {
+      this.columnName = columnName;
+    }
+
+    @Override
+    public TimeValues mapRow(ResultSet resultSet, int i) throws SQLException {
+      TimeValues tv = new TimeValues();
+      tv.setTime(resultSet.getLong("Time"));
+      String valueString = resultSet.getString(columnName);
+      if (valueString != null) {
+        if (TRUE_STR.equalsIgnoreCase(valueString)) {
+          tv.setValue(1);
+        } else if (FALSE_STR.equalsIgnoreCase(valueString)) {
+          tv.setValue(0);
+        } else {
+          try {
+            tv.setValue(resultSet.getFloat(columnName));
+          } catch (Exception e) {
+            tv.setValue(0);
+          }
+        }
+      }
+      return tv;
+    }
+  }
+
+}
---
+++ tsfile/src/main/java/org/apache/iotdb/tsfile/write/chunk/ChunkWriterImpl.java
@@ -0,0 +1,344 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iotdb.tsfile.write.chunk;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import org.apache.iotdb.tsfile.common.conf.TSFileDescriptor;
+import org.apache.iotdb.tsfile.exception.write.PageException;
+import org.apache.iotdb.tsfile.file.header.ChunkHeader;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+import org.apache.iotdb.tsfile.file.metadata.statistics.Statistics;
+import org.apache.iotdb.tsfile.utils.Binary;
+import org.apache.iotdb.tsfile.write.page.PageWriter;
+import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
+import org.apache.iotdb.tsfile.write.writer.TsFileIOWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A implementation of {@code IChunkWriter}. {@code ChunkWriterImpl} consists of a {@code
+ * ChunkBuffer}, a {@code PageWriter}, and two {@code Statistics}.
+ *
+ * @see IChunkWriter IChunkWriter
+ */
+public class ChunkWriterImpl implements IChunkWriter {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ChunkWriterImpl.class);
+
+  private MeasurementSchema measurementSchema;
+
+  /**
+   * help to encode data of this series.
+   */
+  private final ChunkBuffer chunkBuffer;
+
+  /**
+   * value writer to encode data.
+   */
+  private PageWriter pageWriter;
+
+  /**
+   * page size threshold.
+   */
+  private final long pageSizeThreshold;
+
+  private final int maxNumberOfPointsInPage;
+
+  // initial value for this.valueCountInOnePageForNextCheck
+  private static final int MINIMUM_RECORD_COUNT_FOR_CHECK = 1500;
+
+  /**
+   * value count in a page. It will be reset after calling {@code writePageHeaderAndDataIntoBuff()}
+   */
+  private int valueCountInOnePage;
+  private int valueCountInOnePageForNextCheck;
+
+  /**
+   * statistic on a stage. It will be reset after calling {@code writeAllPagesOfSeriesToTsFile()}
+   */
+  private Statistics<?> chunkStatistics;
+
+  /**
+   * statistic on a page. It will be reset after calling {@code writePageHeaderAndDataIntoBuff()}
+   */
+  private Statistics<?> pageStatistics;
+
+  // time of the latest written time value pair, we assume data is written in time order
+  private long maxTimestamp;
+  private long minTimestamp = Long.MIN_VALUE;
+
+  /**
+   * @param schema schema of this measurement
+   */
+  public ChunkWriterImpl(MeasurementSchema schema) {
+    this.measurementSchema = schema;
+    this.chunkBuffer = new ChunkBuffer(measurementSchema);
+
+    this.pageSizeThreshold = TSFileDescriptor.getInstance().getConfig().getPageSizeInByte();
+    this.maxNumberOfPointsInPage = TSFileDescriptor.getInstance().getConfig().getMaxNumberOfPointsInPage();
+    // initial check of memory usage. So that we have enough data to make an initial prediction
+    this.valueCountInOnePageForNextCheck = MINIMUM_RECORD_COUNT_FOR_CHECK;
+
+    // init statistics for this series and page
+    this.chunkStatistics = Statistics.getStatsByType(measurementSchema.getType());
+    this.pageStatistics = Statistics.getStatsByType(measurementSchema.getType());
+
+    this.pageWriter = new PageWriter();
+
+    this.pageWriter.setTimeEncoder(measurementSchema.getTimeEncoder());
+    this.pageWriter.setValueEncoder(measurementSchema.getValueEncoder());
+  }
+
+  @Override
+  public void write(long time, long value) {
+    this.maxTimestamp = time;
+    ++valueCountInOnePage;
+    pageWriter.write(time, value);
+    pageStatistics.updateStats(value);
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = time;
+    }
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long time, int value) {
+    this.maxTimestamp = time;
+    ++valueCountInOnePage;
+    pageWriter.write(time, value);
+    pageStatistics.updateStats(value);
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = time;
+    }
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long time, boolean value) {
+    this.maxTimestamp = time;
+    ++valueCountInOnePage;
+    pageWriter.write(time, value);
+    pageStatistics.updateStats(value);
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = time;
+    }
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long time, float value) {
+    this.maxTimestamp = time;
+    ++valueCountInOnePage;
+    pageWriter.write(time, value);
+    pageStatistics.updateStats(value);
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = time;
+    }
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long time, double value) {
+    this.maxTimestamp = time;
+    ++valueCountInOnePage;
+    pageWriter.write(time, value);
+    pageStatistics.updateStats(value);
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = time;
+    }
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long time, Binary value) {
+    this.maxTimestamp = time;
+    ++valueCountInOnePage;
+    pageWriter.write(time, value);
+    pageStatistics.updateStats(value);
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = time;
+    }
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long[] timestamps, int[] values, int batchSize) {
+    this.maxTimestamp = timestamps[batchSize - 1];
+    valueCountInOnePage += batchSize;
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = timestamps[0];
+    }
+    pageWriter.write(timestamps, values, batchSize);
+    pageStatistics.updateStats(values, batchSize);
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long[] timestamps, long[] values, int batchSize) {
+    this.maxTimestamp = timestamps[batchSize - 1];
+    valueCountInOnePage += batchSize;
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = timestamps[0];
+    }
+    pageWriter.write(timestamps, values, batchSize);
+    pageStatistics.updateStats(values, batchSize);
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long[] timestamps, boolean[] values, int batchSize) {
+    this.maxTimestamp = timestamps[batchSize - 1];
+    valueCountInOnePage += batchSize;
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = timestamps[0];
+    }
+    pageWriter.write(timestamps, values, batchSize);
+    pageStatistics.updateStats(values, batchSize);
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long[] timestamps, float[] values, int batchSize) {
+    this.maxTimestamp = timestamps[batchSize - 1];
+    valueCountInOnePage += batchSize;
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = timestamps[0];
+    }
+    pageWriter.write(timestamps, values, batchSize);
+    pageStatistics.updateStats(values, batchSize);
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long[] timestamps, double[] values, int batchSize) {
+    this.maxTimestamp = timestamps[batchSize - 1];
+    valueCountInOnePage += batchSize;
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = timestamps[0];
+    }
+    pageWriter.write(timestamps, values, batchSize);
+    pageStatistics.updateStats(values, batchSize);
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  @Override
+  public void write(long[] timestamps, Binary[] values, int batchSize) {
+    this.maxTimestamp = timestamps[batchSize - 1];
+    valueCountInOnePage += batchSize;
+    if (minTimestamp == Long.MIN_VALUE) {
+      minTimestamp = timestamps[0];
+    }
+    pageWriter.write(timestamps, values, batchSize);
+    pageStatistics.updateStats(values, batchSize);
+    checkPageSizeAndMayOpenANewPage();
+  }
+
+  /**
+   * check occupied memory size, if it exceeds the PageSize threshold, flush them to given
+   * OutputStream.
+   */
+  private void checkPageSizeAndMayOpenANewPage() {
+    if (valueCountInOnePage == maxNumberOfPointsInPage) {
+      LOG.debug("current line count reaches the upper bound, write page {}", measurementSchema);
+      writePage();
+    } else if (valueCountInOnePage >= valueCountInOnePageForNextCheck) { // need to check memory size
+      // not checking the memory used for every value
+      long currentPageSize = pageWriter.estimateMaxMemSize();
+      if (currentPageSize > pageSizeThreshold) { // memory size exceeds threshold
+        // we will write the current page
+        LOG.debug(
+            "enough size, write page {}, pageSizeThreshold:{}, currentPateSize:{}, valueCountInOnePage:{}",
+            measurementSchema.getMeasurementId(), pageSizeThreshold, currentPageSize, valueCountInOnePage);
+        writePage();
+        valueCountInOnePageForNextCheck = MINIMUM_RECORD_COUNT_FOR_CHECK;
+      } else {
+        // reset the valueCountInOnePageForNextCheck for the next page
+        valueCountInOnePageForNextCheck = (int) (((float) pageSizeThreshold / currentPageSize)
+            * valueCountInOnePage);
+      }
+    }
+  }
+
+  /**
+   * flush data into {@code IChunkWriter}.
+   */
+  private void writePage() {
+    try {
+      chunkBuffer.writePageHeaderAndDataIntoBuff(pageWriter.getUncompressedBytes(),
+          valueCountInOnePage, pageStatistics, maxTimestamp, minTimestamp);
+
+      // update statistics of this series
+      this.chunkStatistics.mergeStatistics(this.pageStatistics);
+    } catch (IOException e) {
+      LOG.error("meet error in pageWriter.getUncompressedBytes(),ignore this page:", e);
+    } catch (PageException e) {
+      LOG.error(
+          "meet error in chunkBuffer.writePageHeaderAndDataIntoBuff, ignore this page:", e);
+    } finally {
+      // clear start time stamp for next initializing
+      minTimestamp = Long.MIN_VALUE;
+      valueCountInOnePage = 0;
+      pageWriter.reset();
+      this.pageStatistics = Statistics.getStatsByType(measurementSchema.getType());
+    }
+  }
+
+  @Override
+  public void writeToFileWriter(TsFileIOWriter tsfileWriter) throws IOException {
+    sealCurrentPage();
+    chunkBuffer.writeAllPagesOfSeriesToTsFile(tsfileWriter, chunkStatistics);
+    chunkBuffer.reset();
+    // reset series_statistics
+    this.chunkStatistics = Statistics.getStatsByType(measurementSchema.getType());
+  }
+
+  @Override
+  public long estimateMaxSeriesMemSize() {
+    return pageWriter.estimateMaxMemSize() + chunkBuffer.estimateMaxPageMemSize();
+  }
+
+  @Override
+  public long getCurrentChunkSize() {
+    // return the serialized size of the chunk header + all pages
+    return ChunkHeader.getSerializedSize(measurementSchema.getMeasurementId()) + chunkBuffer
+        .getCurrentDataSize();
+  }
+
+  @Override
+  public void sealCurrentPage() {
+    if (valueCountInOnePage > 0) {
+      writePage();
+    }
+  }
+
+  @Override
+  public int getNumOfPages() {
+    return chunkBuffer.getNumOfPages();
+  }
+
+  public ChunkBuffer getChunkBuffer() {
+    return chunkBuffer;
+  }
+
+  @Override
+  public TSDataType getDataType() {
+    return measurementSchema.getType();
+  }
+}
